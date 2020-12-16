0d00f9e4cc4b11f4560e6bc1defea375b6cace80
Artem Chernenko <ac.chernenko@gmail.com>
2020-12-16 21:10:53 +0700

    the breakdown by module and create a function template

---
+++ src/functions.py
@@ -0,0 +1,44 @@
+def add_user(dictionary: dict):
+    """
+    Add new user
+    :param dictionary:
+    :return:dictionary
+    """
+    
+    pass
+
+
+def delete_user(dictionary: dict):
+    """
+    Deleted user
+    :param dictionary:
+    :return: dictionary
+    """
+    pass
+
+
+def update_user(dictionary: dict):
+    """
+    Update user number
+    :param dictionary:
+    :return: dictionary
+    """
+    pass
+
+
+def search_user(dictionary: dict):
+    """
+    Search user (Name or number)
+    :param dictionary:
+    :return: dictionary
+    """
+    pass
+
+
+def print_phonebook(dictionary: dict):
+    """
+    Print all phonebook
+    :param dictionary:
+    :return: None
+    """
+    pass
